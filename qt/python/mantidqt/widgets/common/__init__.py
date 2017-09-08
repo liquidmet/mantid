cbadd1a7f524d4ad9cd1cc315754b9d953b7a908
Martyn Gigg <martyn.gigg@gmail.com>
2017-09-08 16:13:08 +0100

    Rename classes from sip inside the module __init__.py
    Current versions of sip insist on placing names within classes defined
    by their C++ namespaces. Newer versions of sip can hide these but they
    will be painful to make available everywhere.
    Refs #20381

--- qt/python/mantidqt/widgets/common/__init__.py
+++ qt/python/mantidqt/widgets/common/__init__.py
@@ -1,9 +1,11 @@
 """ Defines the api for the common subpackage of mantidqt.widgets
 """
-from __future__ import absolute_import
+from __future__ import (absolute_import, unicode_literals)
 
-from ._common import *
+from . import _common
 
-
-# Define import all
+# Define import all API
 __all__ = ["DesktopServices"]
+
+# Handle renaming to string the silly namespace->class translation that sip does
+DesktopServices = _common.MantidQt.API.MantidDesktopServices
--- qt/python/mantidqt/widgets/common/tests/desktopservices_test.py
+++ qt/python/mantidqt/widgets/common/tests/desktopservices_test.py
@@ -1,12 +1,18 @@
 from __future__ import (absolute_import, division,
                         print_function, unicode_literals)
 
+import inspect
 import unittest
 
 class DesktopServicesTest(unittest.TestCase):
 
     def test_class_exists_with_expected_name(self):
         from mantidqt.widgets.common import DesktopServices # noqa
+        self.assertTrue(inspect.isclass(DesktopServices))
+
+    def test_method_exists_on_class(self):
+        from mantidqt.widgets.common import DesktopServices # noqa
+        self.assertTrue(hasattr(DesktopServices, 'openUrl'))
 
 
 if __name__ == '__main__':
